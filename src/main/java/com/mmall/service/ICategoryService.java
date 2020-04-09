a7cf9063fbeb4db8430c5cee5e0cc70aeae9bc32
Tian <litiansq89@gmail.com>
2020-04-09 16:30:13 -0400

    商品分类模块

---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,17 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+
+    ServerResponse<String> addCategory(String categoryName, Integer parentId);
+
+    ServerResponse<String> updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+}
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -192,4 +192,14 @@ public class UserServiceImpl implements IUserService {
         return ServerResponse.createBySuccess(user);
     }
 
+    // backend
+
+    public ServerResponse checkAdminRole(User user) {
+        if(user != null && user.getRole().intValue() == Const.Role.ROLE_ADMIN) {
+            return ServerResponse.createBySuccess();
+        } else {
+            return ServerResponse.createByError();
+        }
+    }
+
 }
